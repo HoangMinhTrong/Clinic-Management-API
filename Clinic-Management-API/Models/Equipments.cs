9015b602653dbe8c57707fdcfdbdb1895d9bc50c
HoangMinhTrong <tronggaw2@gmail.com>
2021-08-27 20:51:04 +0700

    Add-Models

---
+++ Clinic-Management-API/Models/Equipments.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Clinic_Management_API.Models
+{
+    public class Equipments
+    { 
+        [Key]
+       public int equip_id { get; set; } 
+       public string equip_name { get; set; }
+       public string requested_date { get; set; }
+       public string date_defected { get; set; }
+    }
+}
